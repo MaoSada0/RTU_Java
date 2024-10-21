54f86112ca22a6a62ace1e71c880049d55232a39
qqMao <666kakryatskii666@gmail.com>
2024-10-21 23:30:26 +0300

    add task 7

---
+++ src/ru/qq/practice_7/FibonacciTwoThread.java
@@ -0,0 +1,40 @@
+package ru.qq.practice_7;
+
+public class FibonacciTwoThread {
+    private static long result;
+
+    public static void getFibonacciNum(int n) {
+
+        Thread sumThread = new Thread(() -> fibonacciFunc(n));
+
+        Thread printThread = new Thread(() -> {
+            try {
+                sumThread.join();
+                System.out.println("Res: " + result);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        printThread.start();
+
+        sumThread.start();
+
+    }
+
+    private static void fibonacciFunc(int n){
+        System.out.println("First thread started");
+        if (n <= 1) {
+            result = n;
+        } else {
+            long a = 0, b = 1;
+            for (int i = 2; i <= n; i++) {
+                long next = a + b;
+                a = b;
+                b = next;
+            }
+            result = b;
+        }
+        System.out.println("First thread ended");
+    }
+}
---
+++ src/ru/qq/practice_7/Main.java
@@ -0,0 +1,9 @@
+package ru.qq.practice_7;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+
+        FibonacciTwoThread.getFibonacciNum(n);
+    }
+}
